904f6abe476725f786040650afe0263979b22c0b
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-22 00:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var result = document.getElementById("timer");
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate()+1;
+// time
+var hour = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+var formattedDate = `${month}/${day}/${year},`
+var formattedTime = `${hour}:${minutes}:${seconds}`
+result.textContent = formattedDate + formattedTime + " PM";
